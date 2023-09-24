25d43ed3fe534cfba863ac4cb729c921cf8b7b07
qiaotao <2294073149@qq.com>
2023-09-24 21:27:44 +0800

    1.创建gateway网关模块创建，并测试完成路由转发。

---
+++ gulimall-gateway/src/test/java/com/atguigu/gulimall/gateway/GulimallGatewayApplicationTests.java
@@ -0,0 +1,16 @@
+package com.atguigu.gulimall.gateway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class GulimallGatewayApplicationTests { //qiaotao-p:这行不加public，那么则install报错，而别的项目可以不加。
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
